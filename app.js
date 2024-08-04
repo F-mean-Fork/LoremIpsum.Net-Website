591680c9a97955f47bb46d4c0426a7374f69a9b3
Ivan Lukiev <ivan.lukiev@mail.ru>
2024-08-05 00:42:04 +0300

    added 2 destop vers

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const percentValue = document.querySelector('.wrapper__contentWrap__from-wrap-in
     const header = document.querySelector('.header')
     window.onscroll = () => {
         if (document.documentElement.scrollTop > 80) {
-            header.classList.add('header_active');
+            header.classList.add('header__active');
         } else {
-            header.classList.remove('header_active');
+            header.classList.remove('header__active');
         }
     };
 }());
@@ -22,3 +22,18 @@ rangeInput.addEventListener('input', () => {
 });
 
 rangeInput.dispatchEvent(new Event('input'));
+
+
+// Burger-styles
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header__wrapper__nav');
+    const menuCloseItem = document.querySelector('.header__wrapper__nav__close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header__wrapper__nav__active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header__wrapper__nav__active');
+    });
+}())
